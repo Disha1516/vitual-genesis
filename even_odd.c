55ccae9ec52fb99504750b3f18fd3212fd8d687a
Disha1516 <59725564+Disha1516@users.noreply.github.com>
2020-01-10 14:15:18 +0530

    Update even_odd.c

--- even_odd.c
+++ even_odd.c
@@ -5,9 +5,9 @@ int main() {
     scanf("%d", &num);
     // True if num is perfectly divisible by 2
     if(num % 2 == 0)
-        printf("%d is even.", num);
+       printf("%d is even.", num);
     else
-        printf("%d is odd.", num);
+       printf("%d is odd.", num);
     
     return 0;
 }
